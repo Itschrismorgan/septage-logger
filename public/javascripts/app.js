bfab17a8654cca93173d95e47c992f6bfa8f0227
Chris Morgan <chris@itsthemorgans.com>
2015-04-06 03:22:44 +0000

    initial commit

---
+++ public/javascripts/app.js
@@ -0,0 +1,20 @@
+var septageLogger = angular.module('septageLogger',['ngRoute']);
+
+
+septageLogger.controller('MainCtrl',['$scope', function($scope){
+}]);
+
+septageLogger.controller('IndexCtrl',['$scope',function($scope){
+    $scope.name = 'Free Code Camp';
+    
+}]);
+
+
+septageLogger.config(function($routeProvider) {
+$routeProvider.
+when('/', {
+templateUrl: 'javascripts/views/home.html',
+controller: 'IndexCtrl'
+}).
+otherwise('/');
+});
