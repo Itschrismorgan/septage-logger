823984dbf9bf3d4a3b839c541dca58477fbb8b90
Chris Morgan <chris@itsthemorgans.com>
2015-05-11 23:40:25 -0400

    minor changes to driver portal to get it to show a truck list.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -258,6 +258,32 @@ septageLogger.controller('UserCtrl',['$scope', '$routeParams', 'userService', 'c
 }]);
 
 
+septageLogger.controller('DriverCtlr', ['$scope', '$routeParams', '$http', 'truckService', 'userService', function($scope, $routeParams, $http, truckService, userService){
+
+    userService.getUser($routeParams.username)
+        .then(function(data){
+            $scope.username = data.data.username;
+            $scope.accountType = data.data.type;
+            $scope.company = data.data.company;
+            reloadTruckList();
+        }, function(error){
+            console.log("problem");
+        });
+
+
+    function reloadTruckList(){
+        $scope.truckList = [];
+        truckService.getTruckList()
+            .then(function(response){
+                response.data.map(function(truck){
+                    $scope.truckList.push(truck);
+                });
+            }, function(error){
+                console.log(error);
+            });
+    }
+}]);
+
 septageLogger.service('companyService', ['$http', function($http){
     
     
@@ -445,7 +471,7 @@ when('/user/:username',{
 }).
 when('/driver/:username',{
     templateUrl: 'javascripts/views/driver.html',
-    controller: 'UserCtrl'
+    controller: 'DriverCtlr'
 }).
 otherwise('/');
 });
