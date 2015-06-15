eead18d7d25fb752f2c0d528fba78a0b35edcba2
Luke <lukukas@yahoo.com>
2015-06-15 19:07:43 +0000

    company name added to report

--- routes/reports.js
+++ routes/reports.js
@@ -11,7 +11,7 @@ router.get('/', function(req,res,next){
     //console.log(req.user);
     if (req.user || req.user.type === "admin" || req.user.type === "contractor"){
         //console.log("test it");
-        reports.listTrucksAndCollections(req.user, function(err, data){
+        reports.listTrucksAndCollections(req.user, res);/*{
             //console.log("did this work?");
            if(err){
                 console.log(err);
@@ -20,7 +20,7 @@ router.get('/', function(req,res,next){
             }
 
             res.status(200).json(data); 
-        });
+        });*/
     } else {
         res.status(401).json({code: 401, message: 'You are not authorized to access this resource'});
     }
--- server/controllers/reports.js
+++ server/controllers/reports.js
@@ -7,7 +7,7 @@ var truck = mongoose.model('Truck');
 var company = mongoose.model('Company');
 var collection = mongoose.model('Collection');
 
-exports.listTrucksAndCollections = function(user, cb){
+exports.listTrucksAndCollections = function(user, res){
     console.log("in get Collections list for report");
     console.log(user);
     var query = {};
@@ -17,10 +17,37 @@ exports.listTrucksAndCollections = function(user, cb){
     console.log(query);
     collection.find(query, function(err, collections){
         if(err){
-            cb({code: 400, message: err.message}, null);
-            return;
+            res.status(500).json({code:500, message: "GetUser: Server error"});
         }
-
-        cb(null, collections);
+        
+        var modCollections = [];
+        
+        collections.forEach(function (element, index){
+            var modCollection = {};
+            modCollection._id = collections[index]._id;
+            modCollection.companyId = collections[index].companyId;
+            modCollection.driverId = collections[index].driverId;
+            modCollection.locationType = collections[index].locationType;
+            modCollection.truckId = collections[index].truckId;
+            modCollection.volume = collections[index].volume;
+            modCollection.location = collections[index].location;
+            modCollection.type = collections[index].type;
+            modCollection.createdTimeStamp = collections[index].createdTimeStamp;
+        
+            company.findOne({_id: collections[index].companyId}, function(err, company){
+                if (err){
+                    console.log(err);
+                }
+                modCollection.companyName = company.name;  
+                modCollections.push(modCollection);
+                if (index === collections.length - 1) {
+                    res.status(200).json(modCollections);
+                }
+            });
+        });
+        
+        
+        //console.log(modCollections);
+        //cb(null, collections);
     });
 };
